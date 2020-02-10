36c79149018bf1c31e77cd0c3e87cbc8d8ad075c
diegodeveloper <diegodeveloper@outlook.com.br>
2020-02-10 19:51:27 -0400

    migration tradutor

--- etraducao/Migrations/20200210235102_Tradutor.Designer.cs
+++ etraducao/Migrations/20200210235102_Tradutor.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using etraducao.Data;
 
 namespace etraducao.Migrations
 {
     [DbContext(typeof(EtraducaoContexto))]
-    partial class EtraducaoContextoModelSnapshot : ModelSnapshot
+    [Migration("20200210235102_Tradutor")]
+    partial class Tradutor
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ etraducao/Migrations/20200210235102_Tradutor.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace etraducao.Migrations
+{
+    public partial class Tradutor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "TradutorId",
+                table: "Solicitacao",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Tradutor",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    Nome = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tradutor", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Solicitacao_TradutorId",
+                table: "Solicitacao",
+                column: "TradutorId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Solicitacao_Tradutor_TradutorId",
+                table: "Solicitacao",
+                column: "TradutorId",
+                principalTable: "Tradutor",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Solicitacao_Tradutor_TradutorId",
+                table: "Solicitacao");
+
+            migrationBuilder.DropTable(
+                name: "Tradutor");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Solicitacao_TradutorId",
+                table: "Solicitacao");
+
+            migrationBuilder.DropColumn(
+                name: "TradutorId",
+                table: "Solicitacao");
+        }
+    }
+}
